f5a3fdb8ef474ed61e26ed6eaa419756f8b22dfc
TsT <tst2005@gmail.com>
2015-08-09 21:54:42 +0200

    try to support classic

---
+++ classic-featured.lua
@@ -0,0 +1,27 @@
+local class = require "classic"
+
+local common = {}
+common.class = function(name, prototype, parent)
+	local klass = class():extend():new()
+	local parent = parent or class():extend()
+--nil,parent):extend(nil,prototype)
+	klass.init = (prototype or {}).init or (parent or {}).init
+	klass.name = name
+	return klass
+end
+common.instance = function(class, ...)
+        return class:new(...)
+end
+common.__BY = "classic"
+
+local _M = setmetatable({}, {
+	__call = function(_, ...)
+		return common.class(...)
+	end,
+	__index = common,
+	__newindex = function() error("read-only", 2) end,
+	__metatable = false,
+})
+
+--pcall(function() require("i"):register("30log", _M) end)
+return _M
