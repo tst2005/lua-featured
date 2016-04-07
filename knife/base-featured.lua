da3bc829dd30b0bd366c2d0ae20f2b424db05542
TsT <tst2005@gmail.com>
2016-04-07 14:32:19 +0200

    add new class system implementation: knife.base

---
+++ knife/base-featured.lua
@@ -0,0 +1,31 @@
+local base = require "knife.base"
+
+local common = {}
+common.class = function(name, prototype, parent)
+	local parent = parent or base:extend()
+	local klass = parent:extend(prototype or {})
+	klass.init = (prototype or {}).init or (parent or {}).init
+	klass.constructor = klass.init
+	klass.name = name
+	klass.new = klass.new or common.instance
+	return klass
+end
+common.instance = function(class, ...)
+        return class(...)
+end
+common.__BY = "knife.base"
+common.new = common.class
+
+local _M = setmetatable({}, {
+	__call = function(_, ...)
+		return common.class(...)
+	end,
+	__index = common,
+	__newindex = function() error("read-only", 2) end,
+	__metatable = false,
+	__class = common.class,
+	__instance = common.instance,
+})
+
+--pcall(function() require("i"):register("30log", _M) end)
+return _M
